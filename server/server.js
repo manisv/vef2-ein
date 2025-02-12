5d16fd50327f8f6830ba5fb19aba611fb1a7d93e
manisv <54678497+manisv@users.noreply.github.com>
2025-02-12 14:17:17 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,38 +3,34 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
-app.use(cors()); // Enable CORS
+app.use(cors());
 
-// Load the JSON data
 const ghibliData = require('./ghibli-data.json');
 
-// Endpoint to get all films
 app.get('/api/films', (req, res) => {
   res.json(ghibliData.films);
 });
 
-// Endpoint to get details of a specific film by ID
 app.get('/api/films/:id', (req, res) => {
   const filmId = req.params.id;
   const film = ghibliData.films.find(f => f.id === filmId);
 
   if (film) {
-    // Fetch related characters, locations, species, and vehicles
     const characters = film.people
       .map(url => ghibliData.people.find(p => p.url === url))
-      .filter(Boolean); // Remove undefined values
+      .filter(Boolean);
 
     const locations = film.locations
       .map(url => ghibliData.locations.find(l => l.url === url))
-      .filter(Boolean); // Remove undefined values
+      .filter(Boolean);
 
     const species = film.species
       .map(url => ghibliData.species.find(s => s.url === url))
-      .filter(Boolean); // Remove undefined values
+      .filter(Boolean);
 
     const vehicles = film.vehicles
       .map(url => ghibliData.vehicles.find(v => v.url === url))
-      .filter(Boolean); // Remove undefined values
+      .filter(Boolean);
 
     res.json({
       ...film,
